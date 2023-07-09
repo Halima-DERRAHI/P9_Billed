cddea5478c5e1c58d0d0bf23daa7550b14e19707
Halima-DERRAHI <123747796+Halima-DERRAHI@users.noreply.github.com>
2023-07-09 15:52:16 +0100

    fix 2 bugs

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -22,11 +22,7 @@ export default class NewBill {
     const fileName = filePath[filePath.length-1]
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
-  // const fileType = /^.*\.(jpg|JPG|png)$/
-
-  //  if (file.type.match(fileType)) {
     formData.append('file', file)
-  //  } 
     formData.append('email', email)
 
     this.store
